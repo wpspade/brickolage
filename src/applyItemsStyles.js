dc0ee96f150f63bdb05b54f6b9cf0b3b5823c3fa
Yauhen <support@wpspade.com>
2019-09-06 00:08:56 +0300

    Removed extra height variable

--- src/applyItemsStyles.js
+++ src/applyItemsStyles.js
@@ -1,4 +1,4 @@
-export default function applyItemsStyles( { sortedOrder, items, height } ) {
+export default function applyItemsStyles( { sortedOrder, items } ) {
 
 	sortedOrder.map( ( col, i ) => col.map( num => items[ num ].style.order = i + 1 ) );
 }
